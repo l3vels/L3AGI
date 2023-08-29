4889d0d02c3a74b64f6c6cebf7c309538ccbacda
Giga Chkhikvadze <Giga.Chkhikvadze@gmail.com>
2023-08-29 18:28:59 +0400

    feat:  exception for models

---
+++ apps/server/exceptions.py
@@ -0,0 +1,71 @@
+class AppBaseException(Exception):
+    pass
+
+class AgentException(AppBaseException):
+    pass
+
+class AgentNotFoundException(AgentException):
+    pass
+
+
+class ConfigException(AppBaseException):
+    pass
+
+class InvalidConfigException(ConfigException):
+    pass
+
+
+class DatasourceException(AppBaseException):
+    pass
+
+class DatasourceNotFoundException(DatasourceException):
+    pass
+
+class ToolException(AppBaseException):
+    pass
+
+class ToolNotFoundException(DatasourceException):
+    pass
+
+
+class ConfigException(AppBaseException):
+    pass
+
+class ConfigNotFoundException(DatasourceException):
+    pass
+
+class AccountException(AppBaseException):
+    pass
+
+class AccountNotFoundException(DatasourceException):
+    pass
+
+class UserException(AppBaseException):
+    pass
+
+class UserNotFoundException(DatasourceException):
+    pass
+
+class RoleException(AppBaseException):
+    pass
+
+class RoleNotFoundException(DatasourceException):
+    pass
+
+class UserAccountException(AppBaseException):
+    pass
+
+class UserAccountNotFoundException(DatasourceException):
+    pass
+
+class AccountException(AppBaseException):
+    pass
+
+class AccountNotFoundException(DatasourceException):
+    pass
+
+class ProjectException(AppBaseException):
+    pass
+
+class ProjectNotFoundException(DatasourceException):
+    pass
