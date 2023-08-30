9b50522715d2217ad793a1a1db33f89619003b39
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-08-30 17:12:12 +0400

    feat: team model

---
+++ apps/server/models/team_agent.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import List, Optional
+import uuid
+
+from sqlalchemy import Column, String, Boolean, UUID, func, or_, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
+from l3_types.team_types import teamInput
+from exceptions import teamNotFoundException
+
+class TeamAgentModel(BaseModel):
+    """
+    Represents an team entity.
+
+    Attributes:
+        id (UUID): Unique identifier of the team.
+        name (str): Name of the team.
+
+    """
+    __tablename__ = 'team_agent'
+
+    id = Column(UUID, primary_key=True, index=True, default=uuid.uuid4)
+    team_id = Column(UUID, ForeignKey('team.id'), nullable=True) #gonna use if need store teams based on game, project or any
+    agent_id = Column(UUID, ForeignKey('agent.id'), nullable=False)
+    
+    agent = relationship("AgentModel", back_populates="agent", cascade="all, delete")
+    team = relationship("TeamModel", back_populates="team", cascade="all, delete")
