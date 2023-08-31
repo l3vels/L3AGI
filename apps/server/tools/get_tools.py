31c06ae05a0dd56b692d9754b4ece6828829a83b
Mirian Okradze <okradzemirian@gmail.com>
2023-08-31 22:36:05 +0400

    feat: add openweathermap toolkit

--- apps/server/tools/get_tools.py
+++ apps/server/tools/get_tools.py
@@ -4,41 +4,60 @@ from tools.duck_duck_go.duck_duck_go_search_toolkit import DuckDuckGoSearchToolk
 from tools.bing.bing_search_toolkit import BingSearchToolkit
 from tools.wikipedia.wikipedia_search_toolkit import WikipediaSearchToolkit
 from tools.arxiv.arxiv_search_toolkit import ArxivSearchToolkit
+from tools.open_weather_map.open_weather_map_toolkit import OpenWeatherMapToolkit
 
-TOOLKITS = [SerpGoogleSearchToolkit(), WebScraperToolkit(), DuckDuckGoSearchToolkit(), BingSearchToolkit(), WikipediaSearchToolkit(), ArxivSearchToolkit()]
+TOOLKITS = [
+    SerpGoogleSearchToolkit(),
+    WebScraperToolkit(),
+    DuckDuckGoSearchToolkit(),
+    BingSearchToolkit(),
+    WikipediaSearchToolkit(),
+    ArxivSearchToolkit(),
+    OpenWeatherMapToolkit(),
+]
+
 
 def get_all_tools():
     """Return a list of all tools."""
     result = []
 
     for toolkit in TOOLKITS:
-        result.append({
-            "toolkit_id": toolkit.toolkit_id,
-            "is_system": True,
-            "is_active": True,
-            "name": toolkit.name,
-            "description": toolkit.description,
-            "fields": [{
-                "label": env_key.label,
-                "key": env_key.key,
-                "type": str(env_key.key_type),
-                "is_required": env_key.is_required,
-                "is_secret": env_key.is_secret
-            } for env_key in toolkit.get_env_keys()],
-            "tools": [{
-                "tool_id": tool.tool_id,
-                "name": tool.name,
-                "description": tool.description,
-            } for tool in toolkit.get_tools()]
-        })
+        result.append(
+            {
+                "toolkit_id": toolkit.toolkit_id,
+                "is_system": True,
+                "is_active": True,
+                "name": toolkit.name,
+                "description": toolkit.description,
+                "fields": [
+                    {
+                        "label": env_key.label,
+                        "key": env_key.key,
+                        "type": str(env_key.key_type),
+                        "is_required": env_key.is_required,
+                        "is_secret": env_key.is_secret,
+                    }
+                    for env_key in toolkit.get_env_keys()
+                ],
+                "tools": [
+                    {
+                        "tool_id": tool.tool_id,
+                        "name": tool.name,
+                        "description": tool.description,
+                    }
+                    for tool in toolkit.get_tools()
+                ],
+            }
+        )
 
     return result
+
 
 def get_tools(tool_names: str):
     """Return a list of tools."""
     tools = []
 
     # for tool_name in tool_names:
-        # tools.append(TOOLS[tool_name])
+    # tools.append(TOOLS[tool_name])
 
     return tools
---
+++ apps/server/tools/open_weather_map/open_weather_map.py
@@ -0,0 +1,39 @@
+from typing import Optional, Type
+from pydantic import BaseModel, Field
+from langchain.utilities import OpenWeatherMapAPIWrapper
+from langchain.callbacks.manager import (
+    CallbackManagerForToolRun,
+)
+from tools.base import BaseTool
+
+
+class OpenWeatherMapSchema(BaseModel):
+    query: str = Field(
+        ...,
+        description="The search query for DuckDuckGo search.",
+    )
+
+
+class OpenWeatherMapTool(BaseTool):
+    """Tool that queries the OpenWeatherMap API."""
+
+    name = "OpenWeatherMap Search"
+    
+    description = (
+        "A wrapper around OpenWeatherMap API. "
+        "Useful for fetching current weather information for a specified location. "
+        "Input should be a location string (e.g. London,GB)."
+    )
+
+    args_schema: Type[OpenWeatherMapSchema] = OpenWeatherMapSchema
+
+    tool_id = "47a7e8c6-49f2-4b8d-8ba4-8879099e1be2"
+
+    def _run(
+        self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Search DuckDuckGo and return the results."""
+        search = OpenWeatherMapAPIWrapper()
+        search.openweathermap_api_key = self.get_env_key("OPENWEATHERMAP_API_KEY")
+        return search.run(query)
+
