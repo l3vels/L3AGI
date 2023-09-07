f65d512d03e2e72e4e399991cb05d165cc2fba4a
levanion <levanion1998@gmail.com>
2023-09-07 18:27:10 +0400

    fix: replace urls with image assets

--- apps/ui/src/pages/Datasource/constants.ts
+++ apps/ui/src/pages/Datasource/constants.ts
@@ -1,31 +1,33 @@
+import postgresImg from 'assets/images/postgres.png'
+import mySqlImg from 'assets/images/mySql.png'
+import fileImg from 'assets/images/file.jpg'
+import webImg from 'assets/images/web.jpg'
+import notionImg from 'assets/images/notion.png'
+import shopifyImg from 'assets/images/shopify.png'
+
 export const DATA_LOADER_IMAGES = [
   {
     sourceName: 'Postgres',
-    imageSrc:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/800px-Postgresql_elephant.svg.png',
+    imageSrc: postgresImg,
   },
   {
     sourceName: 'MySQL',
-    imageSrc:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStwXG33lFYL4EOU3XEdsb0gT3e14xyOuwC4A&usqp=CAU',
+    imageSrc: mySqlImg,
   },
   {
     sourceName: 'Upload File',
-    imageSrc:
-      'https://static.vecteezy.com/system/resources/previews/012/325/824/original/file-dark-mode-glyph-ui-icon-attach-document-digital-information-user-interface-design-white-silhouette-symbol-on-black-space-solid-pictogram-for-web-mobile-isolated-illustration-vector.jpg',
+    imageSrc: fileImg,
   },
   {
     sourceName: 'Web Page',
-    imageSrc:
-      'https://icon-library.com/images/website-icon-png-transparent/website-icon-png-transparent-9.jpg',
+    imageSrc: webImg,
   },
   {
     sourceName: 'Notion',
-    imageSrc:
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b86edfa2-993d-4f6a-87e4-d0709fd9c0b1/ddgxeer-4754f58f-17b1-4d33-b8e3-b158a10a9aba.png/v1/fill/w_1280,h_1280/white_notion_logo_icon_for_black_dark_theme_by_seyahdoo_ddgxeer-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcL2I4NmVkZmEyLTk5M2QtNGY2YS04N2U0LWQwNzA5ZmQ5YzBiMVwvZGRneGVlci00NzU0ZjU4Zi0xN2IxLTRkMzMtYjhlMy1iMTU4YTEwYTlhYmEucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.cumez1G4ONxtJEtwuYDFMWxdL-VUReQXufSovBCVI14',
+    imageSrc: notionImg,
   },
   {
     sourceName: 'Shopify',
-    imageSrc: 'https://assets.stickpng.com/images/58482ec0cef1014c0b5e4a70.png',
+    imageSrc: shopifyImg,
   },
 ]
