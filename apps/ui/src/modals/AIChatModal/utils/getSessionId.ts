43063975926584ba5042a410c386919187340a31
Mirian Okradze <okradzemirian@gmail.com>
2023-09-11 11:17:52 +0400

    fix: chat session id in frontend

--- apps/ui/src/modals/AIChatModal/utils/getSessionId.ts
+++ apps/ui/src/modals/AIChatModal/utils/getSessionId.ts
@@ -1,17 +1,29 @@
+import { Nullable } from 'types'
+
 const getSessionId = ({
   user,
   account,
   isPrivateChat,
+  agentId,
+  teamId,
 }: {
   user: any
   account: any
   isPrivateChat: boolean
+  agentId: Nullable<string>
+  teamId: Nullable<string>
 }) => {
   if (isPrivateChat) {
     // If chat is private
+
+    if (agentId) return `${agentId}-${user.id}`
+    if (teamId) return `${teamId}-${user.id}`
     return `${account.id}-${user.id}`
   } else {
     // If chat is team
+
+    if (agentId) return agentId
+    if (teamId) return teamId
     return account.id
   }
 }
