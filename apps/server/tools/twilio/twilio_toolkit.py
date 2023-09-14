da0a4eb42136d1ade5091d6a4a79f9942d826281
Mirian Okradze <okradzemirian@gmail.com>
2023-09-14 17:17:03 +0400

    feat: add twilio send tool

--- apps/server/tools/twilio/twilio_toolkit.py
+++ apps/server/tools/twilio/twilio_toolkit.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from typing import List
-from tools.base import BaseToolkit, BaseTool, ToolEnvKey
+from tools.base import BaseToolkit, BaseTool, ToolEnvKey, ToolEnvKeyType
 from tools.twilio.twilio_send import TwilioSendTool
 
 class TwilioSearchToolkit(BaseToolkit, ABC):
@@ -9,10 +9,12 @@ class TwilioSearchToolkit(BaseToolkit, ABC):
     slug: str = "twilio"
     toolkit_id = "8dcb3215-a3ec-4da6-a673-60e4af61e86d"
 
-    is_active = False
-
     def get_tools(self) -> List[BaseTool]:
         return [TwilioSendTool()]
     
     def get_env_keys(self) -> List[ToolEnvKey]:
-        return []
+        return [
+            ToolEnvKey(label="Account SID", key="TWILIO_ACCOUNT_SID", key_type=ToolEnvKeyType.STRING, is_required=True, is_secret=False),
+            ToolEnvKey(label="Auth Token", key="TWILIO_AUTH_TOKEN", key_type=ToolEnvKeyType.STRING, is_required=True, is_secret=True),
+            ToolEnvKey(label="From Number", key="TWILIO_FROM_NUMBER", key_type=ToolEnvKeyType.STRING, is_required=True, is_secret=False),
+        ]
