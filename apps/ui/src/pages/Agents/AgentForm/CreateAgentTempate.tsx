430f3328f3852442cc2194f03cf73eaa24f646f6
levanion <levanion1998@gmail.com>
2023-09-14 13:36:20 +0400

    fix: create from template fetch issue

--- apps/ui/src/pages/Agents/AgentForm/CreateAgentTempate.tsx
+++ apps/ui/src/pages/Agents/AgentForm/CreateAgentTempate.tsx
@@ -24,7 +24,7 @@ import { StyledButtonWrapper } from './CreateAgentForm'
 import CreateAgentButtonCard from './components/CreateAgentButtonCard'
 
 const CreateAgentTemplate = () => {
-  const { agentsData } = useAgents()
+  const { agentsData, refetchAgent } = useAgents()
 
   const navigate = useNavigate()
 
@@ -67,7 +67,8 @@ const CreateAgentTemplate = () => {
                   onViewClick={() => navigate(`/agents/${agent.id}`)}
                   headerTag={agent.role}
                   onChatClick={() => navigate(`/copilot?agent=${agent.id}`)}
-                  onCreateClick={() => {
+                  onCreateClick={async () => {
+                    await refetchAgent({ id: agent.id })
                     navigate(`/agents/create-agent?agentId=${agent.id}`)
                   }}
                 />
