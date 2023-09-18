8b3dff766defa9299aafba5de96f59f9d2293222
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-09-18 13:25:22 +0400

    feat: show youtube links

--- apps/ui/src/modals/AIChatModal/components/ChatMessageList/components/AiMessageMarkdown.tsx
+++ apps/ui/src/modals/AIChatModal/components/ChatMessageList/components/AiMessageMarkdown.tsx
@@ -1,8 +1,9 @@
-import styled from 'styled-components'
-import ReactMarkdown from 'react-markdown'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import remarkGfm from 'remark-gfm'
+import React from 'react';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import remarkGfm from 'remark-gfm';
 
 const AiMessageMarkdown = ({ children }: { children: any }) => {
   return (
@@ -11,10 +12,28 @@ const AiMessageMarkdown = ({ children }: { children: any }) => {
       remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
       components={{
         table: ({ node, ...props }) => <StyledTable {...props} />,
+        a: ({ href, children }) => {
+          const youtubeRegex = /^https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)&/;
 
+          if (youtubeRegex.test(href as string)) {
+            const videoId = (href as string).match(youtubeRegex)?.[1];
+            return (
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            );
+          }
+
+          return <a href={href as string}>{children}</a>;
+        },
         code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || 'language-js')
-
+          const match = /language-(\w+)/.exec(className || 'language-js');
           return !inline && match ? (
             <SyntaxHighlighter
               children={String(children).replace(/\n$/, '')}
@@ -27,21 +46,21 @@ const AiMessageMarkdown = ({ children }: { children: any }) => {
             <code className={className} {...props}>
               {children}
             </code>
-          )
+          );
         },
       }}
     />
-  )
-}
+  );
+};
 
-export default AiMessageMarkdown
+export default AiMessageMarkdown;
 
 const StyledReactMarkdown = styled(ReactMarkdown)`
   color: #fff;
   display: flex;
   flex-direction: column;
   gap: 10px;
-`
+`;
 
 const StyledTable = styled.table`
   border-collapse: collapse;
@@ -52,4 +71,4 @@ const StyledTable = styled.table`
     padding: 5px 30px;
     text-align: center;
   }
-`
+`;
