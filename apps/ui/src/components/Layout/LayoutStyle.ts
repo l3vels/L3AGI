875ff11363a7edce9f9577779a2204eec96fa74b
jelo kokhodze <jelo.kokhodze@gmail.com>
2023-09-19 10:33:22 +0400

    Update LayoutStyle.ts

--- apps/ui/src/components/Layout/LayoutStyle.ts
+++ apps/ui/src/components/Layout/LayoutStyle.ts
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const StyledAppContainer = styled.div`
-  background: ${({ theme }) => theme.body.testVariableColor};
-  /* background: linear-gradient(265.15deg, rgba(76, 166, 248, 1) -32.37%, rgba(33, 82, 243, 1) 100%); */
+  /* background: ${({ theme }) => theme.body.testVariableColor}; */
+  background: linear-gradient(265.15deg, rgba(76, 166, 248, 1) -32.37%, rgba(33, 82, 243, 1) 100%);
   height: 100vh;
   display: flex;
   flex-direction: column;
