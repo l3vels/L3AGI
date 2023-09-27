f221c378bc73dd8c52cf43ed65004cb193c4a0d4
Mirian Okradze <okradzemirian@gmail.com>
2023-09-27 18:36:37 +0400

    feat: add different index type and response mode in data source

--- apps/server/datasources/file/file_retriever.py
+++ apps/server/datasources/file/file_retriever.py
@@ -4,18 +4,29 @@ from pathlib import Path
 import shutil
 import s3fs
 from uuid import uuid4
-from llama_index import SummaryIndex, SimpleDirectoryReader, ServiceContext
+from llama_index import SummaryIndex, VectorStoreIndex, TreeIndex, SimpleDirectoryReader, ServiceContext
 from llama_index import load_index_from_storage, StorageContext
 from llama_index.llms import LangChainLLM
 from langchain.chat_models import ChatOpenAI
 from config import Config
 from services.aws_s3 import AWSS3Service
 from typings.config import AccountSettings
+from enum import Enum
 
 s3 = s3fs.S3FileSystem(
    key=Config.AWS_ACCESS_KEY_ID,
    secret=Config.AWS_SECRET_ACCESS_KEY,
 )
+
+class ResponseMode(Enum):
+    REFINE = 'refine'
+    TREE_SUMMARIZE = 'tree_summarize'
+    SIMPLE_SUMMARIZE = 'simple_summarize'
+
+class IndexType(Enum):
+    SUMMARY = 'summary'
+    VECTOR_STORE = 'vector_store'
+    TREE = 'tree'
 
 class FileDatasourceRetriever:
     datasource_id = None
@@ -23,14 +34,16 @@ class FileDatasourceRetriever:
     index_persist_dir: str
     datasource_path: Path
     service_context: ServiceContext
+    index_type: str
+    response_mode: str
 
-
-    def __init__(self, settings: AccountSettings, account_id: str, datasource_id: str) -> None:
+    def __init__(self, settings: AccountSettings, index_type: str, response_mode: str, account_id: str, datasource_id: str) -> None:
         self.datasource_id = datasource_id
         self.datasource_path = Path(f"tmp/datasources/{self.datasource_id}")
+        self.index_type = index_type
+        self.response_mode = response_mode
         
         self.index_persist_dir = f"{Config.AWS_S3_BUCKET}/account_{account_id}/index/datasource_{self.datasource_id}"
-        print(self.index_persist_dir)
 
         llm = LangChainLLM(llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=settings.openai_api_key))
         self.service_context = ServiceContext.from_defaults(llm=llm)
@@ -41,7 +54,12 @@ class FileDatasourceRetriever:
         documents = SimpleDirectoryReader(self.datasource_path.resolve()).load_data()
 
         # Create index from documents
-        self.index = SummaryIndex.from_documents(documents, service_context=self.service_context)
+        if self.index_type == IndexType.SUMMARY.value:
+            self.index = SummaryIndex.from_documents(documents, service_context=self.service_context)
+        elif self.index_type == IndexType.VECTOR_STORE.value:
+            self.index = VectorStoreIndex.from_documents(documents, service_context=self.service_context)
+        elif self.index_type == IndexType.TREE.value:
+            self.index = TreeIndex.from_documents(documents, service_context=self.service_context)
 
         # Persist index to S3
         self.index.set_index_id(self.datasource_id)
@@ -66,7 +84,6 @@ class FileDatasourceRetriever:
             AWSS3Service.download_file(key, absolute_path)
 
     def query(self, query_str):
-        query_engine = self.index.as_query_engine(response_mode="tree_summarize", service_context=self.service_context)
+        query_engine = self.index.as_query_engine(response_mode=self.response_mode, service_context=self.service_context)
         result = query_engine.query(query_str)
         return result
-    
