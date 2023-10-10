002a6a80f22585b01d0af65670014d9651beaa8b
Mirian Okradze <okradzemirian@gmail.com>
2023-10-10 13:40:50 +0400

    build: setup pre commit hook, run lint staged in ui

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ git config core.hooksPath .hooks
 
 if [[ "$OSTYPE" != "msys" && "$OSTYPE" != "cygwin" && "$OSTYPE" != "win32" ]]; then
   chmod +x .hooks/commit-msg
+  chmod +x .hooks/pre-commit
 fi
 
 echo "Git hooks have been configured!"
