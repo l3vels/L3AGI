ec3b689aa0b78f9d7a14ec8248ccbe53f87def69
DavitBakhutashvili <davit.bakhutashvili1@gmail.com>
2023-10-17 15:15:51 +0400

    fix: add props and remove unused code

--- apps/ui/src/assets/svgComponents/SortIcon.svg.tsx
+++ apps/ui/src/assets/svgComponents/SortIcon.svg.tsx
@@ -1,8 +1,14 @@
-import * as React from 'react'
 import { SVGProps } from 'react'
 
 const SortIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <svg
+    width='10'
+    height='6'
+    viewBox='0 0 10 6'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
     <path
       d='M1.57129 1.28564L4.5958 4.31016C4.81896 4.53331 5.18077 4.53331 5.40392 4.31016L8.42843 1.28565'
       stroke='white'
