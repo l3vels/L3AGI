324cab05a82136fe1246c1084c38afd8adee7502
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-10-17 22:09:54 +0400

    feat: config based on account

--- apps/server/utils/account.py
+++ apps/server/utils/account.py
@@ -2,84 +2,19 @@ from models.account import AccountModel
 from typing import List
 from typings.account import AccountOutput
 from utils.type import convert_value_to_type
-
-default_configs = {
-    "info": {
-        "logo": '',
-        "welcomeMessage": "Welcome to L3AGI"
-    },
-    "naming": {
-        "home": 'Home',
-        "agent": "Agent",
-        "team": "Team",
-        "datasource": "Data sources",
-        "models": "Models",
-        "discovery": "Discovery",
-        "chat": "Multi-Agent",
-        "toolkits": "Toolkits",
-        "schedules": "Schedules"
-    },
-    "modules": {
-        "home": {
-            "welcome_message": "Build your ",
-            "submodules": {
-                "team": {
-                    "operations": {
-                        "create": True,
-                        "list": True,
-                        "edit": True,
-                        "delete": True
-                    }
-                },
-                "agent": True,
-                "discovery": True,
-            }
-        },
-        "chat" :{
-            "active": True,
-            "label": "Multi-Agent",
-            "submodules": {
-                "team": {
-                    "operations": {
-                        "create": True,
-                        "list": True,
-                        "edit": True
-                    }
-                },
-                "agent": True,
-                "session": True
-            }
-        },
-        "model" : {
-            "active" : True,
-            "label": "Models",
-            "submodules": {
-                "models": {
-                    "operations": {
-                        "create": True,
-                        "list": True,
-                        "edit": True,
-                    }
-                },
-                "fine-tuning": {
-                    "operations": True
-                }
-            }
-        },
-        "toolkit": True, #True
-        "datasource": True,
-        "discovery": False,
-        "Session": False,
-        "schedule": False
-    },
-}
-
+from utils.configs.default import default_configs
+from utils.configs.scrapper import scrapper_configs
 
 
 def convert_model_to_response(account_model: AccountModel) -> AccountOutput:
     account_data = {}
     # if not account_model.configs:
-    account_model.configs = default_configs
+    if account_model.name == 'scraper':
+        account_model.configs = scrapper_configs
+    elif account_model.name == 'heyyou':
+        account_model.configs = scrapper_configs
+    else:
+        account_model.configs = default_configs
         
     # Extract attributes from AccountModel using annotations of Account
     for key in AccountOutput.__annotations__.keys():
