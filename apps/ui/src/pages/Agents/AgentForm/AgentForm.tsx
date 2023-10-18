acc93fea926af929ecea7e5c53b881aa196e7676
levanion <levanion1998@gmail.com>
2023-10-18 11:05:24 +0400

    fix: create agent memory leak

--- apps/ui/src/pages/Agents/AgentForm/AgentForm.tsx
+++ apps/ui/src/pages/Agents/AgentForm/AgentForm.tsx
@@ -49,10 +49,10 @@ const AgentForm = ({ formik }: AgentFormProps) => {
   const { modelOptions, datasourceOptions, toolOptions } = useAgentForm(formik)
 
   useEffect(() => {
-    if (agent_model === '' && modelOptions?.length > 1) {
+    if (agent_model === '' && modelOptions?.length > 0) {
       setFieldValue('agent_model', modelOptions[2].value)
     }
-  }, [values, modelOptions])
+  }, [agent_model])
 
   return (
     <StyledRoot>
