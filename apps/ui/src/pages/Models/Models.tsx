f9e7b40a6921b18044d8c90eabe96a1809ad7ff8
levanion <levanion1998@gmail.com>
2023-10-26 15:29:17 +0400

    feat: add fine-tuning

--- apps/ui/src/pages/Models/Models.tsx
+++ apps/ui/src/pages/Models/Models.tsx
@@ -16,10 +16,23 @@ import { ButtonPrimary, ButtonTertiary } from 'components/Button/Button'
 import Button from '@l3-lib/ui-core/dist/Button'
 import Typography from '@l3-lib/ui-core/dist/Typography'
 import TypographyPrimary from 'components/Typography/Primary'
+import { useCreateFineTuningService } from 'services/fineTuning/useCreateFineTuningService'
+import { useFineTuningsService } from 'services/fineTuning/useFIneTuningsService'
 
 const Models = ({ isPublic }: { isPublic?: boolean }) => {
   const { t } = useTranslation()
   const { data: models } = useModelsService()
+
+  const [createFineTuningService] = useCreateFineTuningService()
+  const { data: fineTuningData } = useFineTuningsService()
+
+  const handleCreate = () => {
+    createFineTuningService({
+      name: 'Test',
+      file_url:
+        'https://l3-data-dev.s3.amazonaws.com/account_97651e69-b90d-4b13-beca-114daeb94488/files/dwsample1-json-88840543-05ff-41c3-8cda-eaa384a5e8d3.json',
+    })
+  }
 
   return (
     <StyledSectionWrapper>
@@ -28,7 +41,9 @@ const Models = ({ isPublic }: { isPublic?: boolean }) => {
           <StyledSectionTitle>{`${t('model')}`}</StyledSectionTitle>
           <StyledSectionDescription>{t('model-description')}</StyledSectionDescription>
         </div>
-        {/* <ButtonPrimary size={Button.sizes.SMALL}>Add model</ButtonPrimary> */}
+        <ButtonPrimary size={Button.sizes.SMALL} onClick={handleCreate}>
+          {t('add-fine-tuning')}
+        </ButtonPrimary>
       </StyledHeaderGroup>
 
       <ComponentsWrapper noPadding>
@@ -45,6 +60,18 @@ const Models = ({ isPublic }: { isPublic?: boolean }) => {
                 title={model.name}
                 author={model.provider}
                 logoSrc={logoSrc}
+              />
+            )
+          })}
+          {fineTuningData?.map((fineTuning: any, index: number) => {
+            return (
+              <ModelCard
+                key={index}
+                isReadOnly={isPublic}
+                isDisabled={false}
+                title={fineTuning.name}
+                author={'levan'}
+                logoSrc={''}
               />
             )
           })}
