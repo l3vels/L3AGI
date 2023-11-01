082808bf25186c488a144b79eedd14eb740df6fa
levanion <levanion1998@gmail.com>
2023-11-01 12:32:56 +0400

    feat: voices page

--- apps/ui/src/plugins/contact/pages/Voice/Voices.tsx
+++ apps/ui/src/plugins/contact/pages/Voice/Voices.tsx
@@ -1,10 +1,52 @@
+import ComponentsWrapper from 'components/ComponentsWrapper/ComponentsWrapper'
+import { t } from 'i18next'
+import { StyledCardsWrapper } from 'pages/Agents/Agents'
+import {
+  StyledHeaderGroup,
+  StyledSectionDescription,
+  StyledSectionTitle,
+  StyledSectionWrapper,
+} from 'pages/Home/homeStyle.css'
+import ToolCard from 'pages/Toolkit/components/ToolCard'
 import { useVoicesService } from 'plugins/contact/services/voice/useVoicesService'
+import { useNavigate } from 'react-router-dom'
 
-const Voices = () => {
+const Voices = ({ isPublic }: { isPublic?: boolean }) => {
   const { data: voicesData } = useVoicesService()
   console.log('voicesData', voicesData)
+  const navigate = useNavigate()
 
-  return <div>voices</div>
+  return (
+    <StyledSectionWrapper>
+      <StyledHeaderGroup className='header_group'>
+        <div>
+          <StyledSectionTitle>Voices</StyledSectionTitle>
+        </div>
+      </StyledHeaderGroup>
+
+      <ComponentsWrapper noPadding>
+        <StyledCardsWrapper>
+          {voicesData?.map((voice: any, index: number) => {
+            return (
+              <ToolCard
+                key={index}
+                isReadOnly={isPublic}
+                isDisabled={!voice.is_active && !isPublic}
+                title={voice.name}
+                subTitle={!voice.is_active && !isPublic ? `${t('comingSoon')}` : ''}
+                onClick={() => {
+                  if (isPublic) return
+                  navigate(`/voices/${voice.slug}`)
+                  // openModal({ name: 'toolkit-modal', data: { toolSlug: tool.slug } })
+                }}
+                logoSrc={''}
+              />
+            )
+          })}
+        </StyledCardsWrapper>
+      </ComponentsWrapper>
+    </StyledSectionWrapper>
+  )
 }
 
 export default Voices
