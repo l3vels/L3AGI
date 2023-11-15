2a9b2c539b2795d66c3bf132ab2641b45e0f4ec8
DavitBakhutashvili <davit.bakhutashvili1@gmail.com>
2023-11-15 20:29:59 +0400

    fix: updated Session component

--- apps/ui/src/pages/sessions/Session.tsx
+++ apps/ui/src/pages/sessions/Session.tsx
@@ -1,14 +1,8 @@
 import styled from 'styled-components'
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
 
 import Table from 'components/Table'
 
 import Heading from '@l3-lib/ui-core/dist/Heading'
-
-import columnConfig from './columnConfig'
-
-import Dropdown from '@l3-lib/ui-core/dist/Dropdown'
 
 import TextField from '@l3-lib/ui-core/dist/TextField'
 import {
@@ -16,44 +10,16 @@ import {
   StyledSectionTitle,
   StyledSectionWrapper,
 } from 'pages/Home/homeStyle.css'
-import ComponentsWrapper from 'components/ComponentsWrapper/SessionComponentWrapper'
 import SearchOutline from '@l3-lib/ui-core/dist/icons/SearchOutline'
 import { StyledTableWrapper } from 'plugins/contact/pages/Contact/Contacts'
-import { useChatsService } from 'services/chat/useChatsService'
 
+import ComponentsWrapper from 'components/ComponentsWrapper/ComponentsWrapper'
+import { useSession } from './useSession'
 import SessionDropdown from './SessionDropdown'
-import { useSchedules } from 'pages/Schedule/useSchedules'
-import { useAgentsService } from 'services/agent/useAgentsService'
+import columnConfig from './columnConfig'
+
 const Sessions = () => {
-  const { t } = useTranslation()
-  const { data: chatsData } = useChatsService()
-
-  const { schedules } = useSchedules()
-  const { data: agentsData } = useAgentsService()
-
-  const [searchText, setSearchText] = useState('')
-
-  const mappedData = chatsData?.map((chat: any) => ({
-    id: chat?.id,
-    name: chat?.name,
-    agent_name: chat?.agent?.agent?.name,
-    team_name: chat?.team?.team?.name,
-  }))
-
-  const scheduleOptions = schedules?.map((schedule: any) => ({
-    value: schedule.schedule.name,
-    label: schedule.schedule.name,
-  }))
-
-  const agentOptions = agentsData?.map((agent: any) => {
-    return { value: agent.agent.name, label: agent.agent.name }
-  })
-
-  const filteredData = mappedData?.filter(
-    (row: { name: string; agent_name: string; team_name: string }) =>
-      row.name.toLowerCase().includes(searchText.toLowerCase()) ||
-      row.agent_name.toLowerCase().includes(searchText.toLowerCase()),
-  )
+  const { scheduleOptions, agentOptions, filteredData, searchText, setSearchText } = useSession()
 
   return (
     <StyledSectionWrapper>
@@ -217,76 +183,9 @@ export const StyledGridWrapper = styled.div`
   height: 900px;
 `
 
-const StyledSearch = styled.input`
-  color: ${({ theme }) => theme.body.textColorSecondary} !important;
-  border:  ${({ theme }) => theme.body.sessionDropdownBorder}!important;
-
-  min-width: 300px;
-  height: 48px;
-  border-radius: 8px;FDate
-`
-
 const StyledSessionDropdownWrapper = styled.div`
   width: 300px !important;
   height: 48px !important;
-`
-
-const StyledSessionDropdown = styled(Dropdown)`
-  .css-1762592-control {
-    border: ${({ theme }) => theme.body.sessionDropdownBorder}!important;
-    border-radius: 8px;
-    height: 48px;
-  }
-
-  .css-wxpx7r-menu {
-    background: ${({ theme }) => theme.body.toolkitCardBgColorSecondary};
-    border: ${({ theme }) => theme.body.sessionDropdownBorder}!important;
-    color: ${({ theme }) => theme.body.textColorPrimary} !important;
-  }
-  .primary__wrapper {
-    border: ${({ theme }) => theme.body.sessionDropdownBorder}!important;
-  }
-
-  .css-196qg4a-container {
-    border: ${({ theme }) => theme.body.sessionDropdownBorder}!important;
-  }
-  .css-11unzgr {
-    color: ${({ theme }) => theme.body.textColorPrimary} !important;
-  }
-
-  .menu.dropdown-menu-wrapper.css-19zapvn-menu {
-    // background: ${({ theme }) => theme.body.toolkitCardBgColorSecondary};
-    // color: ${({ theme }) => theme.body.textColorPrimary};
-    border: ${({ theme }) => theme.body.border};
-  }
-
-  .icon_component--clickable {
-    path {
-      fill: ${({ theme }) => theme.body.iconColor};
-    }
-  }
-  .css-196qg4a-container:active,
-  .css-196qg4a-container:focus-within {
-    border: ${({ theme }) => theme.body.sessionDropdownBorder} !important;
-  }
-
-  .css-1aqeloh-singleValue {
-    background: rgb(0, 0, 0);
-    height: 30px;
-    border-radius: 8px;
-  }
-
-  .css-1aqeloh-singleValue {
-    background: ${({ theme }) => theme.body.textColorPrimary};
-  }
-
-  .css-ugu73m-placeholder {
-    color: ${({ theme }) => theme.body.textColorSecondary};
-  }
-
-  .css-1mm2o3n-indicatorContainer {
-    color: ${({ theme }) => theme.body.textColorSecondary};
-  }
 `
 
 const StyledRightSideWrapper = styled.div`
@@ -332,7 +231,7 @@ const StyledSearchInput = styled.input`
 const StyledSearchIcon = styled(SearchOutline)`
   position: absolute;
   top: 50%;
-  right: 10px; /* Adjust this value based on the desired right spacing */
+  right: 10px;
   transform: translateY(-50%);
   path {
     fill: ${({ theme }) => theme.body.iconColor};
