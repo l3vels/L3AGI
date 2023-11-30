cd2ea4f625c4cb87c908dd054542e9cf22b04638
levanion <levanion1998@gmail.com>
2023-11-30 11:21:24 +0400

    fix: youtube video placement

--- apps/ui/src/modals/AIChatModal/components/ChatMessageList/components/AiMessageMarkdown.tsx
+++ apps/ui/src/modals/AIChatModal/components/ChatMessageList/components/AiMessageMarkdown.tsx
@@ -25,18 +25,22 @@ const AiMessageMarkdown = ({ children }: { children: any }) => {
         a: ({ href, children }) => {
           // console.log('href', href)
           // console.log('children', children)
+
           if (YOUTUBE_REGEX.test(href as string)) {
             const videoId = (href as string).match(YOUTUBE_REGEX)?.[1]
             return (
-              <iframe
-                width='560'
-                height='315'
-                src={`https://www.youtube.com/embed/${videoId}`}
-                title='YouTube video player'
-                frameBorder='0'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                allowFullScreen
-              ></iframe>
+              <>
+                <br />
+                <iframe
+                  width='560'
+                  height='315'
+                  src={`https://www.youtube.com/embed/${videoId}`}
+                  title='YouTube video player'
+                  frameBorder='0'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                  allowFullScreen
+                ></iframe>
+              </>
             )
           }
 
