32fa296d59712d43152cda0f799e70f7460a4c55
levanion <levanion1998@gmail.com>
2023-12-14 10:25:56 +0400

    fix: hide pagination

--- apps/ui/src/components/Table/Table.tsx
+++ apps/ui/src/components/Table/Table.tsx
@@ -96,7 +96,7 @@ const Table = ({ columns, data, expand, pagination }: TableProps) => {
           ))}
         </StyledThead>
         <StyledTbody {...getTableBodyProps()}>
-          {currentItems.map((row: any, index: number) => {
+          {rows?.map((row: any, index: number) => {
             prepareRow(row)
             return (
               <StyledTr {...row.getRowProps()} key={index} bodyRow>
@@ -108,7 +108,7 @@ const Table = ({ columns, data, expand, pagination }: TableProps) => {
           })}
         </StyledTbody>
       </StyledTable>
-      {pagination && (
+      {/* {pagination && (
         <PaginationWrapper>
           <PageNumber onClick={handlePrevPage}>
             <StyledNavigationChevronLeft size={16} />
@@ -122,7 +122,7 @@ const Table = ({ columns, data, expand, pagination }: TableProps) => {
             <StyledNavigationChevronRight size={16} />
           </PageNumber>
         </PaginationWrapper>
-      )}
+      )} */}
     </StyledRoot>
   )
 }
