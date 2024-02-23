3b6cb1c6b19c5e4b955cdb7121a54cbe9bdc0f4b
levanion <levanion1998@gmail.com>
2024-02-23 12:42:25 +0400

    fix: voice options loader

--- apps/ui/src/plugins/contact/services/voice/useVoiceOptionsService.ts
+++ apps/ui/src/plugins/contact/services/voice/useVoiceOptionsService.ts
@@ -2,12 +2,16 @@ import { useQuery } from '@apollo/client'
 import VOICE_OPTIONS_GQL from '../../gql/voice/voiceOptions.gql'
 
 export const useVoiceOptionsService = () => {
-  const { data, error, loading, refetch } = useQuery(VOICE_OPTIONS_GQL)
+  const { data, error, loading, refetch } = useQuery(VOICE_OPTIONS_GQL, {
+    notifyOnNetworkStatusChange: true,
+  })
+
+  const isLoading = loading && !data
 
   return {
     data: data?.getVoiceOptions || [],
     error,
-    loading,
+    loading: isLoading,
     refetch,
   }
 }
