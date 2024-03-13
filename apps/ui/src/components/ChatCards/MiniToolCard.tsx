78467be1a08cf3f79666b2835930415fab636a2a
levanion <levanion1998@gmail.com>
2024-03-13 14:46:51 +0400

    feat: delete tooltip on card

--- apps/ui/src/components/ChatCards/MiniToolCard.tsx
+++ apps/ui/src/components/ChatCards/MiniToolCard.tsx
@@ -56,7 +56,7 @@ const MiniToolCard = ({
             icon={() => <StyledDeleteIcon />}
             size={IconButton.sizes?.SMALL}
             kind={IconButton.kinds?.TERTIARY}
-            // ariaLabel='Delete'
+            ariaLabel='Delete'
           />
         )}
       </StyledIconButtonWrapper>
