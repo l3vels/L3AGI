cd3ba71f79c626e58fa324048779324b9c047146
Giga <giga.chkhikvadze@gmail.com>
2024-05-29 18:38:04 +0400

    fix: useSharedAccess

--- apps/ui/src/pages/InviteUsers/useSharedAccess.ts
+++ apps/ui/src/pages/InviteUsers/useSharedAccess.ts
@@ -8,10 +8,6 @@ const useSharedAccess = () => {
     const [cookies] = useCookies<any>([''])
     const { account_id } = cookies
 
-    console.log('cookies', cookies)
-
-    // const selected_account_id = 'ed0fa07b-d5a8-462f-8f81-97f4c54b4eb4'
-
     const handleSelectAccess = (access_account_id: string) => {
         if(access_account_id === account_id) {
             removeAccountId()
@@ -19,7 +15,6 @@ const useSharedAccess = () => {
         }
         setAccountId(access_account_id)
     }
-
 
     const columns = renderSharedColumns({ handleSelectAccess, selected_account_id: account_id })
 
