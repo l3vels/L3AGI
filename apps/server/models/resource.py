0966eb1c3f0239ca77433245a77b962e1943d6dd
changelia <changelia97@gmail.com>
2024-06-07 12:32:50 +0400

    feat: resource model

--- apps/server/models/resource.py
+++ apps/server/models/resource.py
@@ -3,13 +3,40 @@ from sqlalchemy import UUID, Boolean, Column, or_, String, Enum, Numeric
 from sqlalchemy.orm import Session, aliased, relationship
 from sqlalchemy.dialects.postgresql import JSONB
 import uuid
+from typings.resource import (
+    ResourceDiscTypeEnum,
+    ResourceCloudTypeEnum,
+    ResourceStatusEnum,
+    ResourceTypeEnum
+)
 
 
 class ResourceModel(BaseModel):
-    """_summary_
+    """
+    Represents a resource configuration for cloud or on-premise environments.
 
     Args:
-        BaseModel (_type_): _description_
+        BaseModel (Type): Inherits from SQLAlchemy's declarative base model.
+
+    Attributes:
+        id (UUID): Unique identifier for the resource.
+        name (str): Name of the resource. This field is required.
+        display_name (str): Display name for the resource. This field is required.
+        type (ResourceTypeEnum): Type of the resource. This field is required.
+        category (str): Category of the resource. This field is optional.
+        ram (Decimal): Amount of RAM in GB. This field is required.
+        secure_price (Decimal): Price for secure access. This field is optional.
+        one_month_price (Decimal): Price for one month usage. This field is optional.
+        three_month_price (Decimal): Price for three months usage. This field is optional.
+        six_month_price (Decimal): Price for six months usage. This field is optional.
+        max_gpu (Decimal): Maximum GPU allocation. This field is optional.
+        lowest_price (JSONB): JSON object containing the lowest price details. This field is required.
+        status (ResourceStatusEnum): Status of the resource. This field is optional.
+        disc_type (ResourceDiscTypeEnum): Type of disk used in the resource. This field is optional.
+        cloud_type (ResourceCloudTypeEnum): Type of cloud environment. This field is optional.
+        region (str): Geographical region of the resource. This field is optional.
+        cuda_version (Decimal): Version of CUDA supported. This field is optional.
+        is_deleted (bool): Flag indicating if the resource is deleted. Default is False.
     """
 
     __tablename__ = "resource"
@@ -17,33 +44,27 @@ class ResourceModel(BaseModel):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String(255), nullable=False)
     display_name = Column(String(255), nullable=False)
-    type = Column(Enum("cpu", "gpu", name="resource_type"), nullable=False)
+    type = Column(Enum(ResourceTypeEnum), nullable=False)
     category = Column(String, nullable=True)
-    ram = Column(Numeric(precision=5, scale=2), nullable=True)
+    ram = Column(Numeric(precision=5, scale=2), nullable=False)
     secure_price = Column(Numeric(precision=5, scale=2), nullable=True)
     one_month_price = Column(Numeric(precision=5, scale=2), nullable=True)
     three_month_price = Column(Numeric(precision=5, scale=2), nullable=True)
     six_month_price = Column(Numeric(precision=5, scale=2), nullable=True)
     max_gpu = Column(Numeric(precision=5, scale=2), nullable=True)
     lowest_price = Column(JSONB, nullable=False)
-    # { compliance, minMemory, minVcpu, minimumBidPrice, stockStatus, uninterruptablePrice  }
     status = Column(
-        Enum('low', 'high', 'unavailable', name='status_enum'),
+        Enum(ResourceStatusEnum),
         nullable=True
     )
     disc_type = Column(
-        Enum('ssd', 'nvme', 'unavailable', name='disc_type_enum'),
+        Enum(ResourceDiscTypeEnum),
         nullable=True
     )
     cloud_type = Column(
-        Enum(
-            'secure cloud',
-            'community cloud',
-            'unavailable',
-            name='cloud_type_enum'
-        ),
+        Enum(ResourceCloudTypeEnum),
         nullable=True)
-    region = Column(String(255), nullable=False)
+    region = Column(String(255))
     cuda_version = Column(Numeric(precision=5, scale=2), nullable=True)
     is_deleted = Column(Boolean, default=False, index=True)
 
@@ -85,7 +106,7 @@ class ResourceModel(BaseModel):
         return resources
 
     @classmethod
-    def create_user_account_access(
+    def create_resource(
         cls,
         db: Session,
         resource,
