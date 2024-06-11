195d5ec20edaa763bc23965b2ec5b60c328cede4
changelia <changelia97@gmail.com>
2024-06-11 18:06:53 +0400

    feat: get pods

--- apps/ui/src/services/pod/usePodService.ts
+++ apps/ui/src/services/pod/usePodService.ts
@@ -1,1 +1,30 @@
 
+import { useQuery, useMutation } from '@apollo/client'
+import getPodsGql from 'gql/pod/getPods.gql'
+
+
+/**
+ * Fetches the pods data using the `getPodsGql` query and returns the result.
+ *
+ * @return {{ data: Pod[] | undefined, error: ApolloError | null, loading: boolean, refetch: () => void }}
+ */
+export const useGetPods = () => {
+    const { data, error, loading, refetch } = useQuery(getPodsGql)
+
+    try {
+        if (error) {
+            // Handle error here
+            console.error("Error fetching user access:", error)
+        }
+    } catch (error) {
+        // Handle any unexpected errors
+        console.error("An unexpected error occurred:", error)
+    }
+
+    return {
+        data: data?.getPods || [],
+        error,
+        loading,
+        refetch,
+    }
+}
